2c63595d06e0a475852c29b6825ce2387a5c540a
gianlucafalaschi <gian100gian@outlook.it>
2024-04-10 17:17:27 +0200

    image added

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -5,11 +5,16 @@
 </script>
 
 <template>
-    <div class="image-container">
-        <img src="" alt="">
+    <div class="jumbotron-container">
+        <h3>CIAO</h3>
     </div>
 </template>
 
 <style>
-
+    h3 {width: 100%;
+        height: 500px;
+        background-image: url('/public/img/ingmar-h-RWS1FY27DUg-unsplash.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
 </style>
