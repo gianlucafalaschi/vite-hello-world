876223a71df2aa3e238d6eab9bc54b9c9b2fdb76
gianlucafalaschi <gian100gian@outlook.it>
2024-04-10 16:41:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script> 
+  
+  import AppTitle from './components/AppTitle.vue';
+  
+  export default {    
+    components: {
+      AppTitle
+      
+    }
+  }
+</script>
+
+<template>
+      <!-- Qui aggiungo html -->
+      <main>
+        <AppTitle></AppTitle>
+      </main>
+</template>
+
+
+<style scoped>
+  /* Qui aggiungiamo CSS */
+</style>
+
+<!-- Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i 
+  vari passaggi dell'installazione come visti a lezione.
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita 
+"La mia prima app con Vite!"
+Bonus:
+Creare un secondo componente AppHero, che visualizza un jumbotron. -->
